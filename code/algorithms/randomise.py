a831c246219a10619cd5a37a3aaf24ab65ca9892
liesbet <lies.ooghe@gmail.com>
2024-01-18 14:01:14 +0100

    making greedy random algorithm

--- code/algorithms/randomise.py
+++ code/algorithms/randomise.py
@@ -1,18 +1,35 @@
 import random
 
-# must have amino as input for continuity to other algorithm
-def random_assignment(protein):
-    for amino in protein.aminos.values():
-        if amino.i == protein.i_list[-1]:
-            amino.direction = 0
-            amino.change_coordinates()
-            return
-        amino.change_direction(random.choice([-2, -1, 1, 2]))
+def random_assignment_amino(protein, amino):
+    if amino.i == protein.i_list[-1]:
+        amino.direction = 0
         amino.change_coordinates()
+        return
+    amino.change_direction(random.choice([-2, -1, 1, 2]))
+    amino.change_coordinates()
 
-def random_reconfigure_amino(protein, change_amino):
-    change_amino.change_direction(random.choice([-2, -1, 1, 2]))
-    change_amino.change_coordinates()
+def random_assignment_protein(protein):
+    for amino in protein.aminos.values():
+        random_assignment_amino(protein, amino)
+
+def random_redirect_amino(protein, change_amino):
+    """
+    Redirects change_amino to another direction and resets all later aminos.
+    """
+    original_direction = change_amino.direction
+    while change_amino.direction == original_direction:
+        change_amino.change_direction(random.choice([-2, -1, 1, 2]))
     for amino in protein.aminos.values():
         if amino.i > change_amino.i:
-            change_amino.change_coordinates
+            amino.change_coordinates()
+
+def random_redirect_aminos(protein, change_amino):
+    """
+    Redirects change_amino to another direction and updates the coordinates
+    of later aminos accordingly.
+    """
+    change_amino.change_direction(random.choice([-2, -1, 1, 2]))
+    for amino in protein.aminos.values():
+        if amino.i > change_amino.i:
+            amino.change_coordinates()
+            
