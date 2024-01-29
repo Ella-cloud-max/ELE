13cafa5c6cea4e4649d0ab1383108fef77a0e34f
Liesbet Ooghe <liesbet.ooghe@student.uva.nl>
2024-01-29 13:37:04 +0100

    merge conflict main

--- main.py
+++ main.py
@@ -1,13 +1,9 @@
 import sys
 
 from code.classes import protein, amino
-from code.algorithms import randomise
-from code.algorithms import depth_first
+from code.algorithms import baseline, randomise, depth_first
 from code.visualisation import visualisation
-from code.algorithms import baseline
 import greedy
-
-
 
 if __name__ == "__main__":
 
@@ -15,27 +11,33 @@ if __name__ == "__main__":
 
     # ------------- baseline random algorithm -------------
 
+    print("BASELINE")
+
     test_protein = protein.Protein(input_file)
     baseline.baseline_random_protein(test_protein)
 
     while test_protein.check_viability() == False:
         baseline.baseline_random_protein(test_protein)
-    
+
     test_protein.print_output("main/random_baseline.csv")
     visualisation.print_folded_protein(f"output/main/random_baseline.csv")
 
     # ------------- adaptations to random algo -------------
-    
+
+    print("RANDOM+")
+
     test_protein = protein.Protein(input_file)
     randomise.random_assignment_protein(test_protein)
 
     while test_protein.check_viability() == False:
         randomise.random_assignment_protein(test_protein)
-    
+
     test_protein.print_output("main/random_improved.csv")
     visualisation.print_folded_protein(f"output/main/random_improved.csv")
 
     # ------------- greedy-random algorithm -----------
+
+    print("GREEDY RANDOM")
 
     test_protein = protein.Protein(input_file)
     greedy.greedy(test_protein)
@@ -44,15 +46,16 @@ if __name__ == "__main__":
 
     # ------------- depth-first algorithm -------------
 
+    print("DEPTH-FIRST")
+
     test_protein = protein.Protein(input_file)
     depth = depth_first.DepthFirst(test_protein)
 
     # Run the algoritm for x amount of seconds
-    depth.run(5)
+    depth.run("main/depthfirst_output.csv", 10)
 
     print(f"Value of the configuration after Depth First: "
          f"{depth.protein.count_score()}")
 
     depth.protein.print_output("output_ella.csv")
     visualisation.print_folded_protein("output/output_ella.csv")
-
