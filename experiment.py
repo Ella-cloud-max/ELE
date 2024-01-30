d7efcb0b056d41f58e59a43f4f03dc39a9499188
Liesbet Ooghe <lies.ooghe@gmail.com>
2024-01-30 13:40:48 +0100

    trial 4 experiment.py'

--- experiment.py
+++ experiment.py
@@ -4,7 +4,6 @@ import sys
 from code.algorithms import baseline
 from code.classes import protein
 
-input_file = f"proteins/protein1.csv"
 start = time.time()
 n_runs = 0
 list_scores = []
@@ -12,7 +11,7 @@ list_scores = []
 while time.time() - start < 100:
     print(f"run: {n_runs}")
     test_protein = protein.Protein(input_file)
-    result = subprocess.run(["timeout", "5", "python3", "main.py", "proteins/protein1.csv"], capture_output=True)
+    result = subprocess.run(["timeout", "5", "python3", "main.py", "proteins/protein1.csv"], capture_output=True, text = True)
     list_scores.append(result.stdout)
     n_runs += 1
 
