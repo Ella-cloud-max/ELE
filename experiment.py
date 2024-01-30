4e8b76a51b79b759b6deb69677e845627b0460d8
Liesbet Ooghe <lies.ooghe@gmail.com>
2024-01-30 13:45:41 +0100

    experiment add histogram

--- experiment.py
+++ experiment.py
@@ -3,6 +3,7 @@ import time
 import sys
 from code.algorithms import baseline
 from code.classes import protein
+import matplotlib.pyplot as plt
 
 start = time.time()
 n_runs = 0
@@ -10,9 +11,14 @@ list_scores = []
 
 while time.time() - start < 100:
     print(f"run: {n_runs}")
-    test_protein = protein.Protein(input_file)
     result = subprocess.run(["timeout", "5", "python3", "main.py", "proteins/protein1.csv"], capture_output=True, text = True)
-    list_scores.append(result.stdout)
+    list_scores.append(int(result.stdout))
     n_runs += 1
 
-print(list_scores)
+bins = [x + 0.5 for x in set(sorted(list_scores))]
+bins.append(min(bins) - 1)
+bins = sorted(bins)
+
+plt.hist(list_scores, bins)
+plt.xlim(min(list_scores) - 1.5, max(list_scores) + 1.5)
+plt.show()
